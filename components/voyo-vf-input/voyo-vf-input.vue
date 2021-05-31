f95dc00a0fe4496ef834ae83c036957cd6cddf80
zhan <zhantewei@gmail.com>
2021-05-31 14:44:38 +0800

    addtion functional

--- components/voyo-vf-input/voyo-vf-input.vue
+++ components/voyo-vf-input/voyo-vf-input.vue
@@ -1,24 +1,30 @@
 <template>
   <view>
-    <view class="voyo-verification">
+    <view class="voyo-verification"
+          :class="{
+        '__disable':disable
+      }"
+    >
       <view
-        @tap="itemTap"
-        v-for="(i, index) in list"
-        class="voyo-verification-col"
-        :key="index"
+          @tap="itemTap"
+          v-for="(i, index) in list"
+          class="voyo-verification-col"
+          :key="index"
       >
         <view
-          :class="[value.length - 1 === index ? '__active' : '']"
-          class="voyo-verification-col-input"
+            :class="[value.length - 1 === index ? '__active' : '']"
+            class="voyo-verification-col-input"
         >
           {{ i.value }}
         </view>
       </view>
     </view>
     <voyo-keyboard
-      :closeBar="false"
-      @keyPress="onKeyPress"
-      :show="keyboardShow"
+        :closeBar="true"
+        @keyPress="onKeyPress"
+        @showChange="keyboardShowChang"
+        :show="keyboardShow"
+        :use-enter="useEnter"
     ></voyo-keyboard>
   </view>
 </template>
@@ -28,8 +34,20 @@ export default {
   props: {
     count: {
       type: Number,
-      value: 3,
+      default: 4,
     },
+    useEnter:{
+      type: Boolean,
+      default: false
+    },
+    disable:{
+      type: Boolean,
+      default: false
+    },
+    autoOpenKeyboard:{
+      type: Boolean,
+      default :false
+    }
   },
   data() {
     return {
@@ -45,25 +63,33 @@ export default {
         const list = [];
         for (let i = 0; i < v; i++) {
           list.push({
-            value: "",
-            active: false,
+            value: ""
           });
         }
         this.list = list;
       },
     },
   },
+  created(){
+    this.keyboardShow=this.autoOpenKeyboard;
+  },
   methods: {
     itemTap() {
+      if(this.disable)return;
       this.keyboardShow = true;
     },
 
     init() {
       this.value = "";
     },
+    keyboardShowChang(e){
+      this.keyboardShow=e;
+    },
     onKeyPress(e) {
       const length = this.value.length;
-      if (e === "del") {
+      if(e===13){
+        this.$emit("confirm",this.value);
+      }else if (e === "del") {
         if (length === 0) return;
         this.value = this.value.slice(0, -1);
         this.list[length - 1].value = "";
@@ -71,10 +97,14 @@ export default {
         if (length < this.count) {
           this.value += e;
           this.list[length].value = e;
-        } else {
           //completed
+          if(this.value.length>=this.count)  this.$emit("complete",this.value);
         }
       }
+    },
+    clear(){
+      this.value="";
+      this.list=[];
     },
     begin() {
       this.keyboardShow = true;
