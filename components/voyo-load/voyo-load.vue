c3a48546e7e671963af66bd84f3040932e5b1d8b
zhan <zhantewei@gmail.com>
2021-07-24 03:19:03 +0800

    load

--- components/voyo-load/voyo-load.vue
+++ components/voyo-load/voyo-load.vue
@@ -23,7 +23,12 @@
         <!--          class="za za-loading voyo-an-loading _textIcon voyo-color-des"-->
         <!--        ></text>-->
         <view class="_img-wrapper">
-          <image class="_img" :src="loadImg"></image>
+          <!--          #ifdef MP-WEIXIN-->
+          <image class="_img" :src="loadImg" />
+          <!--          #endif-->
+          <!--          #ifdef ENV-H5-->
+          <img class="_img" :src="loadImg" />
+          <!--          #endif-->
         </view>
         <view :class="[$slots.default?'_text':'']">
           <slot></slot>
