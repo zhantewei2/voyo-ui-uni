67358a50472e427fb2528bc2852fd58e7f2ccf20
zhan <zhantewei@gmail.com>
2021-10-26 15:31:46 +0800

    systemInfo

--- src/voyo-components/utils/common.ts
+++ src/voyo-components/utils/common.ts
@@ -1,8 +1,28 @@
 declare var uni: any;
 
-const systemInfo = uni.getSystemInfoSync();
+export interface SystemInfo{
+  language: string;
+  model: string;
+  pixelRatio: number;
+  platform: string;
+  safeArea: Record<any, any>;
+  safeAreaInsets: Record<any, any>;
+  screenHeight: number;
+  screenWidth: number;
+  statusBarHeight: number;
+  system: string;
+  windowBottom: number;
+  windowHeight: number;
+  windowTop: number;
+  windowWidth: number;
+  pageHeaderHeight: number;
+}
+
+const systemInfo:SystemInfo = uni.getSystemInfoSync() as any;
 const platform = systemInfo.platform;
 const pageHeaderHeight = 50;
 const pageHeaderTotalH = pageHeaderHeight + systemInfo.statusBarHeight;
 const isH5=!!window;
+systemInfo["pageHeaderHeight"]=50;
+
 export { systemInfo, platform, pageHeaderHeight, pageHeaderTotalH ,isH5};
